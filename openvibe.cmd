1097d8326f9f38a224d5c26f5f72756e97b300f4
raag0206-design <raag0206@gmail.com>
2026-03-27 22:25:43 -0600

    Delete openvibe.cmd

--- openvibe.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-REM Strip the PATH of other Python installations to avoid SRE mismatch
-set PATH=C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem
-set PYTHONHOME=
-set PYTHONPATH=
-
-REM Run OpenViBE Designer
-"C:\Program Files\openvibe-3.7.0-64bit\bin\openvibe-designer.exe" %*
